04fb282fdd5d3b00cf33e91e8bb7454323c9058c
Alex Turbov <i.zaufi@gmail.com>
2018-04-08 23:38:13 +0300

    Remove out of date file

--- profile.d/konsole_profile.sh
+++
@@ -1,9 +0,0 @@
-#
-# Change current color scheme of running `konsole` session
-#
-
-function set_konsole_color_scheme()
-{
-    local scheme=$1
-    [ -n "${KONSOLE_DBUS_SESSION}" ] && /usr/bin/konsoleprofile colorscheme=$scheme
-}
