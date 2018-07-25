09b5d2f74f51283f427179d2bf30d7fcf89a5108
Alex Turbov <i.zaufi@gmail.com>
2018-07-25 18:00:26 +0300

    Store latest changes

--- profile.d/mc-with-skin.sh
+++ profile.d/mc-with-skin.sh
@@ -5,13 +5,21 @@
 function mc()
 {
     local -r colors=$(tput colors)
-    if [[ -f /etc/mc/skins/default-${colors}.ini ]]; then
+    local skin
+    if [[ ${colors} = 16777216 && -f /etc/mc/skins/default-16M.ini ]]; then
+        skin=/etc/mc/skins/default-16M.ini
+
+    # 256, 16 and 8 color terminals
+    elif [[ -f /etc/mc/skins/default-${colors}.ini ]]; then
         skin=/etc/mc/skins/default-${colors}.ini
+
+    # Ok, try the very default skin
     elif [[ -f /etc/mc/skins/default.ini ]]; then
         skin=/etc/mc/skins/default.ini
     fi
+
     # Midnight Commander chdir enhancement
-    if [ -f /usr/libexec/mc/mc-wrapper.sh ]; then
+    if [[ -f /usr/libexec/mc/mc-wrapper.sh ]]; then
         . /usr/libexec/mc/mc-wrapper.sh ${skin:+--skin=${skin}}
     else
         /usr/bin/mc ${skin:+--skin=${skin}}
